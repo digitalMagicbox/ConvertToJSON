bd77a9ce3bfeb0fdb82f7cabc0083270e590691b
digitalMagicbox <pambox+github@gmail.com>
2019-12-02 22:38:33 +0900

    ディレクトリからのファイルリストアップ処理を削除

--- CsvToJSon.ps1
+++ CsvToJSon.ps1
@@ -4,11 +4,4 @@ Param(
     [string]$csvfilepath,
     [string]$jsonfilepath
 )
-Out-File -FilePath $jsonfilepath -Encoding UTF8 
-
-$list=Get-ChildItem -Path (Join-Path $csvfilepath "*.csv") -Recurse -Name
-foreach ($filename in $list)
-{
-    Write-Host (Join-Path $csvfilepath $filename)
-    Import-CSV (Join-Path $csvfilepath $filename) -Encoding Default | ConvertTo-Json | Out-File -FilePath $jsonfilepath -Encoding UTF8
-}
+Import-CSV $csvfilepath -Encoding Default | ConvertTo-Json | Out-File -FilePath $jsonfilepath -Encoding UTF8
